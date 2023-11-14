b67c2767ad1471aeaa9f42d5c9240062cba73591
milet <1783899680@qq.com>
2023-11-14 08:27:45 +0800

    加入购物车

--- src/main/java/com/soft2242/shop/service/UserShoppingCartService.java
+++ src/main/java/com/soft2242/shop/service/UserShoppingCartService.java
@@ -2,6 +2,8 @@ package com.soft2242.shop.service;
 
 import com.soft2242.shop.entity.UserShoppingCart;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.soft2242.shop.query.CartQuery;
+import com.soft2242.shop.vo.CartGoodsVO;
 
 /**
  * <p>
@@ -12,5 +14,11 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2023-11-09
  */
 public interface UserShoppingCartService extends IService<UserShoppingCart> {
-
+    /**
+     * 添加购物车
+     *
+     * @parm query
+     * @return
+     */
+    CartGoodsVO addShopCart(CartQuery query);
 }
